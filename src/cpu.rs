9d258a15ab8c7dc2dfbf2cfe60dc0020a3adab1a
Aneesh <aneesh1701@gmail.com>
2024-01-05 15:32:01 +0530

    create cpu layout

---
+++ src/cpu.rs
@@ -0,0 +1,25 @@
+pub struct CPU {
+    pub a: u8,
+    pub x: u8,
+    pub y: u8,
+    pub status: u8,
+    pub pc: u16,
+    memory: [u8; 0xFFFF],
+}
+
+impl CPU {
+    pub fn new() -> Self {
+        CPU {
+            a: 0,
+            x: 0,
+            y: 0,
+            status: 0,
+            pc: 0,
+            memory: [0; 0xFFFF],
+        }
+    }
+
+    pub fn interpret(&mut self, program: &[u8]) {
+        todo!();
+    }
+}
